4834f8b7eaaf92085624bf5d5effd0f9a07ced8f
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-08-22 21:05:26 +0200

    Integrations: setup

---
+++ integrations/jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  },
+  reporters: [
+    "default",
+    [
+      "jest-junit", 
+      {
+        suiteName: "jest tests"
+      }
+    ]
+  ],
+};

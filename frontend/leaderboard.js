a0348b5f580b800302c0d5e5467b316dba31c2cc
Mikkel <mtkongsted@gmail.com>
2022-10-10 15:12:53 +0200

    Emergency fixes 2

--- frontend/leaderboard.js
+++ frontend/leaderboard.js
@@ -10,7 +10,12 @@ const leaderboardMain = async () => {
     const body = await res.json()
     leaderboardList.innerHTML = ""
     for (let i = 0; i < body.leaderboard.length; i++) {
-        leaderboardList.innerHTML += "<li> <span class=\"leaderboard-text-container\"><p class=\"leaderboard-text\">" + body.leaderboard[i].username + "</p><p class=\"leaderboard-text\">" + body.leaderboard[i].score + "</p></span></li>"
+        leaderboardList.innerHTML += 
+        `<li>
+            <span class=\"leaderboard-text-container\">
+                <p class=\"leaderboard-text\">${body.leaderboard[i].username}</p><p class=\"leaderboard-text\">${body.leaderboard[i].score}</p>
+            </span>
+        </li>`
     }
 };
 

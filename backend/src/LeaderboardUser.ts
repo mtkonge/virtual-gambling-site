ca2e401c5f8fe57a3b7fb74ee5fd31777a0814c6
Mikkel <mtkongsted@gmail.com>
2022-10-10 14:43:23 +0200

    leaderboard style

--- backend/src/LeaderboardUser.ts
+++ backend/src/LeaderboardUser.ts
@@ -1,5 +1,4 @@
 
-
 export class LeaderboardUser {
     username: string
     score: number
--- frontend/leaderboard.js
+++ frontend/leaderboard.js
@@ -10,7 +10,7 @@ const leaderboardMain = async () => {
     const body = await res.json()
     leaderboardList.innerHTML = ""
     for (let i = 0; i < body.leaderboard.length; i++) {
-        leaderboardList.innerHTML += "<li>" + body.leaderboard[i].username + " " + body.leaderboard[i].score + "</li>"
+        leaderboardList.innerHTML += "<li> <span class=\"leaderboard-text-container\"><p class=\"leaderboard-text\">" + body.leaderboard[i].username + "</p><p class=\"leaderboard-text\">" + body.leaderboard[i].score + "</p></span></li>"
     }
 };
 
